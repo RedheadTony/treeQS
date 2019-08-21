d80adedbe0f00b1a9fd1dee8fe1b6802dbb6c98b
Antony <blablablatony@mail.ru>
2019-08-21 22:53:46 +0700

    refactoring

--- src/sagas/helper.js
+++ src/sagas/helper.js
@@ -21,8 +21,6 @@ export function copyWithChildren(cacheChild, parent) {
 
 export function deleteWithChildren(deletingElement) {
   deletingElement.deleted = true
-  console.log('deletingElement')
-  console.log(deletingElement)
   for (const id in deletingElement.children) {
     if (!deletingElement.children[id].deleted) {
       deleteWithChildren(deletingElement.children[id])
@@ -30,48 +28,104 @@ export function deleteWithChildren(deletingElement) {
   }
 }
 
-export function moveChildrenIntoParent(objLink, parentLink) {
-  Object.keys(objLink).forEach(id => {
-    if (parentLink.parentPath.length < objLink[id].parentPath.length) {
-      const splitPath = []
-      let startPush = false
-      objLink[id].parentPath.split('.').forEach(id => {
-        if (+id === parentLink.id || startPush) {
-          startPush = true
-          splitPath.push(id)
-        }
-      })
-      if (splitPath.length === 1 && +splitPath[0] === parentLink.id) {
-        parentLink.children = { ...parentLink.children, [id]: objLink[id] }
-        delete objLink[id]
-      }
+export function Node(selectedNode) {
+  this.id = selectedNode.id
+  this.value = selectedNode.value
+  this.distantChild = []
+  this.deleted = selectedNode.deleted
+  this.parentId = selectedNode.parent ? selectedNode.parent.id : null
+  this.children = {}
+}
+
+export function isParentForNode(parentId, node) {
+  let path = []
+  let isFound = false
+  const start = (parentId, node) => {
+    path.push(node.id)
+    if (node.id === parentId) {
+      isFound = true
     }
+    if (!isFound && node.parent) start(parentId, node.parent)
+  }
+  start(parentId, node)
+  return path
+}
+
+export function isChildForNode(childId, node) {
+  let isChild = false
+  const start = node => {
+    if (childId === node.id) {
+      isChild = true
+    } else {
+      Object.keys(node.children).forEach(key => {
+        start(node.children[key])
+      })
+    }
+  }
+  start(node)
+  return isChild
+}
+
+export function clone(copiedObj, newObj, parent) {
+  newObj.id = copiedObj.id
+  newObj.value = copiedObj.value
+  newObj.deleted = copiedObj.deleted
+  newObj.parent = parent
+  newObj.children = {}
+
+  Object.keys(copiedObj.children).forEach(childId => {
+    newObj.children[childId] = {}
+    clone(copiedObj.children[childId], newObj.children[childId], newObj)
   })
 }
 
-export function findPlace(objLink, insertableItem) {
-  let placeWasFound = false
-  let valuePlace = objLink
-  for (const id in objLink) {
-    if (placeWasFound) break
-    if (insertableItem.parentPath.length > objLink[id].parentPath.length) {
-      const splitPath = []
-      let startPush = false
-      insertableItem.parentPath.split('.').forEach(tmpId => {
-        if (+tmpId === +id || startPush) {
-          startPush = true
-          splitPath.push(tmpId)
-        }
-      })
-      for (let i = 0; i < splitPath.length; i++) {
-        if (valuePlace[splitPath[i]]) {
-          valuePlace = valuePlace[splitPath[i]].children
-          placeWasFound = true
-        } else {
-          valuePlace = objLink
-        }
-      }
+export function find(root, elementId) {
+  let foundParent = null
+
+  const findElement = obj => {
+    if (obj.id === elementId) {
+      foundParent = obj
+    } else {
+      Object.keys(obj.children).forEach(key => findElement(obj.children[key]))
     }
   }
-  return valuePlace
+
+  const start = () => {
+    Object.keys(root).some(key => {
+      findElement(root[key])
+      if (foundParent) return true
+    })
+  }
+  start()
+  return foundParent
+}
+
+export function findNearestParentId(node, pathToParent, startParent) {
+  let nearestParent = startParent
+  const start = (node, pathToParent) => {
+    let newChild = {}
+    if (!node.children) return
+    Object.keys(node.children).some(child => {
+      if (node.children[child].id === pathToParent[pathToParent.length - 1]) {
+        newChild = node.children[child]
+        pathToParent.pop()
+        return true
+      }
+    })
+    if (newChild.id) {
+      nearestParent = newChild
+      start(newChild, pathToParent)
+    }
+  }
+  start(node, pathToParent, nearestParent)
+  return nearestParent
+}
+
+export function moveChildrenIntoParent(obj, parent) {
+  Object.keys(obj).forEach(key => {
+    if (obj[key].parentId === parent.id) {
+      parent.children[obj[key].id] = obj[key]
+      delete obj[key]
+    }
+  })
 }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -15,119 +15,32 @@ import {
   copyObj,
   copyWithChildren,
   deleteWithChildren,
-  // moveChildrenIntoParent,
-  findPlace
+  find,
+  isParentForNode,
+  findNearestParentId,
+  isChildForNode,
+  moveChildrenIntoParent,
+  clone,
+  Node
 } from './helper'
-
-function Node(selectedNode) {
-  this.id = selectedNode.id
-  this.value = selectedNode.value
-  this.distantChild = []
-  this.deleted = selectedNode.deleted
-  this.parentId = selectedNode.parent ? selectedNode.parent.id : null
-  this.children = {}
-}
-
-const isParentForNode = (parentId, node) => {
-  let path = []
-  let isFinded = false
-  const start = (parentId, node) => {
-    path.push(node.id)
-    if(node.id === parentId) {
-      isFinded = true
-    }
-    if(!isFinded && node.parent) start(parentId, node.parent)
-  }
-  start(parentId, node)
-  return path
-}
-
-const isChildForNode = (childId, node) => {
-  let isChild = false
-  const start = (node) => {
-    console.log('start')
-    console.log(node)
-    if(childId === node.id) {
-      isChild = true
-    } else {
-      Object.keys(node.children).forEach(key => {
-        start(node.children[key])
-      })
-    }
-  }
-  console.log('in func')
-  console.log(node)
-  start(node)
-  return isChild
-}
-
-const clone = (clonableObj, newObj, parent) => {
-
-  newObj.id = clonableObj.id
-  newObj.value = clonableObj.value
-  newObj.deleted = clonableObj.deleted
-  newObj.parent = parent
-  newObj.children = {}
-
-  Object.keys(clonableObj.children).forEach(childId => {
-    newObj.children[childId] = {}
-    clone(clonableObj.children[childId], newObj.children[childId], newObj)
-  })
-}
-
-const find = (root, elementId) => {
-  let findedParent = null
-
-  const findElement = (obj) => {
-    if(obj.id === elementId) {
-      findedParent = obj
-    } else {
-      Object.keys(obj.children).forEach(key => findElement(obj.children[key]))
-    }
-  }
-
-  const start = () => {
-    Object.keys(root).some(key => {
-      findElement(root[key])
-      if(findedParent) return true
-    })
-  }
-  start()
-  return findedParent
-}
-
-const moveChildrenIntoParent = (obj, parent) => {
-  Object.keys(obj).forEach(key => {
-    if(obj[key].parentId === parent.id) {
-      parent.children[obj[key].id] = obj[key]
-      delete obj[key]
-    }
-  })
-}
 
 function* moveToCache({ dispatch }) {
   try {
     const { selectedNode } = yield select(state => state.db)
-    const { cache } = yield select(
-      state => state.cache
-    )
+    const { cache } = yield select(state => state.cache)
     const newCache = yield copyObj(cache)
 
-    const findedParent = selectedNode.parent ? find(newCache, selectedNode.parent.id) : null
+    const foundParent = selectedNode.parent
+      ? yield call(find, newCache, selectedNode.parent.id)
+      : null
 
-    console.log('findedParent')
-    console.log(findedParent)
-
-    console.log('selectedNode')
-    console.log(selectedNode)
-
-    // console.log('findedParent')
-    // console.log(findedParent)
-    //
-    // console.log('findedParent')
-    // console.log(findedParent)
-    if((findedParent && findedParent.children[selectedNode.id]) || newCache[selectedNode.id]) {
-      const element = findedParent ? findedParent.children[selectedNode.id] : newCache[selectedNode.id]
+    if (
+      (foundParent && foundParent.children[selectedNode.id]) ||
+      newCache[selectedNode.id]
+    ) {
+      const element = foundParent
+        ? foundParent.children[selectedNode.id]
+        : newCache[selectedNode.id]
       let message = 'Element already moved.'
       if (element.value !== selectedNode.value) {
         message = `Element already moved. His name is "${element.value}".`
@@ -146,191 +59,59 @@ function* moveToCache({ dispatch }) {
       return
     }
 
-    const findNearestParentId = (node, pathToParent, startParent) => {
-      let nearestParent = startParent
-      const start = (node, pathToParent) => {
-        let newChild = {}
-        if(!node.children) return
-        Object.keys(node.children).some(child => {
-          if(node.children[child].id ===  pathToParent[pathToParent.length - 1]) {
-            newChild = node.children[child]
-            pathToParent.pop()
-            return true
-          }
-        })
-        if(newChild.id) {
-          nearestParent = newChild
-          start(newChild, pathToParent)
-        }
-      }
-      start(node, pathToParent, nearestParent)
-      // console.log('end**************')
-      // console.log(nearestParent)
-      return nearestParent
-    }
-
-    // const node = {
-    //   id: selectedNode.id,
-    //   value: selectedNode.value,
-    //   distantChild: [],
-    //   deleted: selectedNode.deleted,
-    //   parentId: selectedNode.parent ? selectedNode.parent.id : null,
-    //   children: {}
-    // }
-
     const node = new Node(selectedNode)
-    // const node = {
-    //   id: selectedNode.id,
-    //   value: selectedNode.value,
-    //   distantChild: [],
-    //   deleted: selectedNode.deleted,
-    //   parentId: selectedNode.parent ? selectedNode.parent.id : null,
-    //   children: {}
-    // }
-    // const distantChild = []
+
     for (const id in newCache) {
       // поиск родительской ветки
-      const pathToParent = isParentForNode(newCache[id].id, selectedNode)
+      const pathToParent = yield call(
+        isParentForNode,
+        newCache[id].id,
+        selectedNode
+      )
       const lastId = pathToParent.pop()
-      if(lastId === newCache[id].id) {
-        const nearestParent = findNearestParentId(newCache[id], pathToParent, newCache[id])
-        if(nearestParent.deleted) {
+      if (lastId === newCache[id].id) {
+        const nearestParent = yield call(
+          findNearestParentId,
+          newCache[id],
+          pathToParent,
+          newCache[id]
+        )
+        if (nearestParent.deleted) {
           node.deleted = true
         }
         nearestParent.distantChild.push(node.id)
-        // console.log('*******************nearestParent')
-        // console.log(nearestParent)
-        // distantChild.push(nearestParent)
       } else {
-        console.log('no parent')
         // поиск дочерней ветки
-        console.log('isChildForNode')
-        console.log(newCache[id].id)
-        console.log(selectedNode)
-        const isChild = isChildForNode(newCache[id].id, selectedNode)
-        console.log('isChild')
-        console.log(isChild)
-        if(isChild) {
+        const isChild = yield call(
+          isChildForNode,
+          newCache[id].id,
+          selectedNode
+        )
+        if (isChild) {
           node.distantChild.push(newCache[id].id)
         }
       }
-
-      // console.log('distantParent')
-      // console.log(pathToParent)
-      // console.log('nearestParent')
-      // console.log(nearestParent)
     }
 
+    yield call(moveChildrenIntoParent, newCache, node)
 
-
-    console.log('newCache')
-    console.log(newCache)
-
-    moveChildrenIntoParent(newCache, node)
-
-    if(findedParent) {
-      findedParent.children[selectedNode.id] = node
+    if (foundParent) {
+      foundParent.children[selectedNode.id] = node
     } else {
       newCache[selectedNode.id] = node
     }
     yield put(setCache(newCache))
-
-
-
-    // console.log(place)
-
-    // const newCache = yield copyObj(cache)
-    // let valueToAdd = yield copyObj(selectedNode)
-    // if (foundId.length) {
-    //   const valuePlace = yield call(findPlace, newCache, valueToAdd)
-    //   let message = 'Element already moved.'
-    //   if (valueToAdd.value !== valuePlace[valueToAdd.id].value) {
-    //     message = `Element already moved. His name is "${valuePlace[valueToAdd.id].value}".`
-    //   }
-    //   yield put(
-    //     openModal({
-    //       title: message,
-    //       actions: [
-    //         {
-    //           title: 'Ok',
-    //           onClick: () => dispatch(closeModal())
-    //         }
-    //       ]
-    //     })
-    //   )
-    //   return
-    // }
-    //
-    // yield call(moveChildrenIntoParent, newCache, valueToAdd)
-    // const valuePlace = yield call(findPlace, newCache, valueToAdd)
-    // valuePlace[valueToAdd.id] = valueToAdd
-    // const parentNodeDeleted = deletedElementsIds.some(id =>
-    //   valueToAdd.parentPath.split('.').includes(id.toString())
-    // )
-    // if (parentNodeDeleted) {
-    //   valueToAdd.deleted = true
-    // }
-
-    // yield put(addId(valueToAdd.id))
-    // yield put(selectNode({}))
   } catch (e) {
     console.log(e)
   }
 }
-
-// function* moveToCache({ dispatch }) {
-//   try {
-//     const { selectedNode } = yield select(state => state.db)
-//     const { cache, addedToCacheIds, deletedElementsIds } = yield select(
-//       state => state.cache
-//     )
-//     const foundId = addedToCacheIds.filter(id => id === selectedNode.id)
-//
-//     const newCache = yield copyObj(cache)
-//     let valueToAdd = yield copyObj(selectedNode)
-//     if (foundId.length) {
-//       const valuePlace = yield call(findPlace, newCache, valueToAdd)
-//       let message = 'Element already moved.'
-//       if (valueToAdd.value !== valuePlace[valueToAdd.id].value) {
-//         message = `Element already moved. His name is "${valuePlace[valueToAdd.id].value}".`
-//       }
-//       yield put(
-//         openModal({
-//           title: message,
-//           actions: [
-//             {
-//               title: 'Ok',
-//               onClick: () => dispatch(closeModal())
-//             }
-//           ]
-//         })
-//       )
-//       return
-//     }
-//
-//     yield call(moveChildrenIntoParent, newCache, valueToAdd)
-//     const valuePlace = yield call(findPlace, newCache, valueToAdd)
-//     valuePlace[valueToAdd.id] = valueToAdd
-//     const parentNodeDeleted = deletedElementsIds.some(id =>
-//       valueToAdd.parentPath.split('.').includes(id.toString())
-//     )
-//     if (parentNodeDeleted) {
-//       valueToAdd.deleted = true
-//     }
-//     yield put(setCache(newCache))
-//     yield put(addId(valueToAdd.id))
-//     yield put(selectNode({}))
-//   } catch (e) {
-//     console.log(e)
-//   }
-// }
 
 function* addNewElement({ value }) {
   try {
     const { nextId } = yield select(state => state.db)
     const { selectedCacheNode, cache } = yield select(state => state.cache)
     const selectedId = selectedCacheNode.id
-    const valueToAdd = {
+    const newNode = {
       id: nextId,
       parentId: selectedId,
       value,
@@ -338,12 +119,11 @@ function* addNewElement({ value }) {
       distantChild: [],
       children: {}
     }
-    const newCache = copyObj(cache)
-    // const valuePlace = yield call(findPlace, newCache, valueToAdd)
-    const valuePlace = find(newCache, selectedId) || newCache
-    valuePlace.children[valueToAdd.id] = valueToAdd
+    const newCache = yield call(copyObj, cache)
+    const parentNode = yield call(find, newCache, selectedId) || newCache
+    parentNode.children[newNode.id] = newNode
     yield put(setCache(newCache))
-    yield put(addId(valueToAdd.id))
+    yield put(addId(newNode.id))
     yield put(selectNode({}))
     yield put(incrementId())
   } catch (e) {
@@ -354,11 +134,9 @@ function* addNewElement({ value }) {
 function* editElement({ value }) {
   const { selectedCacheNode, cache } = yield select(state => state.cache)
   const selectedId = selectedCacheNode.id
-  const newCache = copyObj(cache)
-  const valuePlace = find(newCache, selectedId) || newCache
-  console.log('valuePlace')
-  console.log(valuePlace)
-  valuePlace.value = value
+  const newCache = yield call(copyObj, cache)
+  const node = yield call(find, newCache, selectedId) || newCache
+  node.value = value
   yield put(setCache(newCache))
   yield put(selectCacheNode({}))
 }
@@ -367,29 +145,16 @@ function* deleteElementSaga() {
   try {
     const { cache, selectedCacheNode } = yield select(state => state.cache)
     const selectedId = selectedCacheNode.id
-    const newCache = copyObj(cache)
-    // const valuePlace = yield call(findPlace, newCache, selectedCacheNode)
-    const valuePlace = find(newCache, selectedId) || newCache
-    valuePlace.deleted = true
-    deleteWithChildren(valuePlace)
-    console.log('valuePlace.distantChild')
-    console.log(valuePlace.distantChild)
+    const newCache = yield call(copyObj, cache)
+    const node = yield call(find, newCache, selectedId) || newCache
+    node.deleted = true
+    yield call(deleteWithChildren, node)
     // хотел в distantChild хранить ссылки, реализовать не вышло
     // видимо создавался новый объект, где - не нашел, обошел через id
-    valuePlace.distantChild.forEach(id => {
+    node.distantChild.forEach(id => {
       const child = find(newCache, id)
       deleteWithChildren(child)
     })
-
-    // deleteWithChildren(valuePlace[selectedId])
-    // for (const id in newCache) {
-    //   const shouldBeDeleted = newCache[id].parentPath
-    //     .split('.')
-    //     .includes(selectedId.toString())
-    //   if (shouldBeDeleted) {
-    //     deleteWithChildren(newCache[id])
-    //   }
-    // }
     yield put(setCache(newCache))
     yield put(selectCacheNode({}))
   } catch (e) {
@@ -401,37 +166,16 @@ function* apply() {
   try {
     const { cache } = yield select(state => state.cache)
     const { dataBase } = yield select(state => state.db)
-    const dbCopy = {'1' : {}}
+    const dbCopy = { '1': {} }
     clone(dataBase['1'], dbCopy[1], null)
-    // console.log('dataBase')
-    // console.log(dataBase)
-    // console.log('dbCopy')
-    // console.log(dbCopy)
-    // console.log('cache')
-    // console.log(cache)
     for (const id in cache) {
-      const element = copyObj(cache[id])
-      // console.log('element')
-      // console.log(element)
-      const elementInDb = find(dbCopy, element.id)
-      // console.log('elementInDb')
-      // console.log(elementInDb)
+      const element = yield call(copyObj, cache[id])
+      const elementInDb = yield call(find, dbCopy, element.id)
       elementInDb.value = element.value
-      // elementInDb.deleted = element.deleted
-      // const element = copyObj(cache[id])
-      // const splitPath = element.parentPath.split('.')
-      // let elementInDb = dbCopy
-      // splitPath.shift()
-      // for (let i = 0; i < splitPath.length; i++) {
-      //   elementInDb = elementInDb[splitPath[i]].children
-      // }
       if (element.deleted) {
-        deleteWithChildren(elementInDb)
+        yield call(deleteWithChildren, elementInDb)
       }
-      // elementInDb[element.id].value = element.value
-      copyWithChildren(element.children, elementInDb)
-      console.log('elementInDb')
-      console.log(elementInDb)
+      yield call(copyWithChildren, element.children, elementInDb)
     }
     yield put(setDataBase(dbCopy))
     yield put(selectNode({}))
